b13922c569f1c7b85bbef44f682836a8424aaa46
Leon200211 <leon200207@yandex.ru>
2022-12-25 14:00:46 +0300

    Сделал первую структуру

---
+++ public_html/application/config/routes.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    '' => [
+        'controller' => 'main',
+        'action' => 'index'
+    ],
+    'account/login' => [
+        'controller' => 'account',
+        'action' => 'login'
+    ],
+    'account/register' => [
+        'controller' => 'account',
+        'action' => 'register'
+    ]
+
+];
+
+
+
