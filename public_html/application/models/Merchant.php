d8c38556e03183cbd231b81ff74c5c86462d21a8
Leon200211 <leon200207@yandex.ru>
2023-01-05 10:47:37 +0300

    Добавил реферальные бонусы

--- public_html/application/models/Merchant.php
+++ public_html/application/models/Merchant.php
@@ -43,27 +43,34 @@ class Merchant extends Model {
 	}
 
 	public function createTariff($data, $tarif) {
+	    // проверка на существование пользователя
 		$dataRef = $this->db->column('SELECT ref FROM accounts WHERE id = :id', ['id' => $data['uid']]);
 		if ($dataRef === false) {
 			return false;
 		}
+
+		// реферальные награды
 		if ($dataRef != 0) {
+		    // расчет бонуса
 			$refSum = round((($data['amount'] * 5) / 100), 2);
 			$params = [
 				'sum' => $refSum,
 				'id' => $dataRef,
 			];
+			// обновление баланса пользователя
 			$this->db->query('UPDATE accounts SET refBalance = refBalance + :sum WHERE id = :id', $params);
 			$params = [
-				'id' => '',
+				'id' => NULL,
 				'uid' => $dataRef,
 				'unixTime' => time(),
 				'description' => 'Реферальное вознаграждение, сумма '.$refSum.' $',
 			];
 			$this->db->query('INSERT INTO history VALUES (:id, :uid, :unixTime, :description)', $params);
 		}
+
+
 		$params = [
-			'id' => '',
+			'id' => NULL,
 			'uid' => $data['uid'],
 			'sumIn' => round($data['amount'], 2),
 			'sumOut' => round($data['amount'] + (($data['amount'] * $tarif['percent']) / 100), 2),
@@ -74,7 +81,7 @@ class Merchant extends Model {
 		$this->db->query('INSERT INTO tariffs VALUES (:id, :uid, :sumIn, :sumOut, :percent, :unixTimeStart, :unixTimeFinish)', $params);
 		
 		$params = [
-			'id' => '',
+			'id' => NULL,
 			'uid' => $data['uid'],
 			'unixTime' => time(),
 			'description' => 'Инвестиция, номер вклада # '.$this->db->lastInsertId(),
